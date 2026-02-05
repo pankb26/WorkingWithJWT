0029a21715151351a76d98ff8252ebcc870123fc
Pankaj Bankar <pankajgb2633@gmail.com>
2026-02-05 18:20:44 +0530

    Added JWTTools for Password Hasher

---
+++ backend/JWTAPI.Tools/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using JWTHLAPI.Helpers;
+
+namespace JWTAPI.Tools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Password Hash Generator ===");
+
+            string passwordHash;
+            string passwordSalt;
+
+            PasswordHasher.CreatePasswordHash(
+                "Admin@123",
+                out passwordHash,
+                out passwordSalt
+            );
+
+            Console.WriteLine("\nPasswordHash:");
+            Console.WriteLine(passwordHash);
+
+            Console.WriteLine("\nPasswordSalt:");
+            Console.WriteLine(passwordSalt);
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
